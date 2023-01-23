2aa138449b2f91e13933c1941691fc2d35d25156
Leonid221 <leonid.natochiy@gmail.com>
2023-01-23 22:40:50 +0200

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+
+    let reverseStr = Math.abs(n).toString().split('').map(int => parseInt(int, 10)).reverse().join('');
+
+    return Number(reverseStr);  
+
 }
